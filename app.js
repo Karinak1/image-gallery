2de78234e8dc5b7935f05221cb2bd02c4d865e21
volto9 <volto9@yahoo.com>
2024-10-12 14:38:45 +0100

    grid

--- app.js
+++ app.js
@@ -5,17 +5,32 @@ const images = [
     src: '1.jpg',
     alt: 'vineyard',
     title: 'vineyard',
+    tabindex: '0',
   },
 
   {
     src: 'Argentina.jpg',
     alt: 'vineyard',
     title: 'vineyard',
+    tabindex: '1',
   },
   {
     src: 'v3.jpg',
     alt: 'vineyard',
     title: 'vineyard',
+    tabindex: '2',
+  },
+  {
+    src: 'vineyard-2.jpg',
+    alt: 'vineyard',
+    title: 'vineyard',
+    tabindex: '3',
+  },
+  {
+    src: '751027.jpg',
+    alt: 'vineyard',
+    title: 'vineyard',
+    tabindex: '4',
   },
 ];
 
@@ -24,6 +39,8 @@ for (let i = 0; i < images.length; i++) {
 
   img.src = images[i].src;
   img.alt = images[i].alt;
+  img.title = images[i].title;
+  img.tabindex = images[i].tabindex;
 
   img.addEventListener('click', function () {
     displayImage.src = images[i].src;
